59ffa4ede4ece8aa37159f5f493856a950b4dada
Bala <87976274+Balahacker@users.noreply.github.com>
2023-03-18 23:02:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const timeE1 = document.getElementById('time');
+const dateE1 = document.getElementById('date');
+const currentWeatherItemE1 = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const countryE1 = document.getElementById('country');
+const weatherForecastE1 = document.getElementById('weather-forecast');
+const currentTempE1 = document.getElementById('current-temp');
+const days= ['Sunday' , 'Monday' , 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday' , 'Saturday'];
+const months = ['Jan' , 'Feb' , 'Mar' , 'Apr' , 'May' , 'Jun' , 'Jul' , 'Aug' , 'sep' , 'Oct' , 'Nov' , 'Dec'];
+
+const API_KEY = '435608c06991b17f7e0721d7ed4d3195';
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes();
+    const ampm = hour >= 12 ? 'PM' : 'AM'
+
+    timeE1.innerHTML = hoursIn12HrFormat + ':' + minutes + '' + `<span id="am-pm">${ampm}</span>`
+
+    dateE1.innerHTML = days[day] + ', ' + date + ' '+ months[month]
+
+
+}, 1000);
+getWeatherData()
+function getWeatherData (){
+    navigator.geolocation.getCurrentPosition((success) =>{
+        let {latitude , longitude} = success.coords;
+        fetch(`https://api.openweathermap.org/data/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data =>{
+            console.log(data)
+            showWeatherData(data);
+
+        })
+
+    })
+}
+function showWeatherData(data){
+    let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+
+    currentWeatherItemE1.innerHTML = `<div class="weather-items">
+        <div>Humidity</div>
+        <div>${humidity}%</div>
+    </div>
+        <div class="weather-items">
+            <div>Pressure</div>
+            <div>121</div>
+        </div>
+        <div class="weather-items">
+            <div>wind speed</div>
+            <div>222</div>
+        </div>`
+
+}
